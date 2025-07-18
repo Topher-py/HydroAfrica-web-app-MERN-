1dc4d0d2569047aa562c89bcb5cc78711708c631
Topher-py <christophermutua2454@gmail.com>
2025-07-18 17:55:27 +0300

    Deployment

--- client/src/components/NgoLogin.jsx
+++ client/src/components/NgoLogin.jsx
@@ -15,7 +15,7 @@ const NgoLogin = ({ onLogin }) => {
     e.preventDefault();
     setError('');
     try {
-      const res = await axios.post('https://hydroafrica.onrender.com/api/reports', form);
+      const res = await axios.post('https://hydroafrica.onrender.com/api/ngos/login', form);
       localStorage.setItem('ngoToken', res.data.token);
       onLogin(res.data.ngo); // parent callback
       navigate('/'); // ✅ redirect to homepage
